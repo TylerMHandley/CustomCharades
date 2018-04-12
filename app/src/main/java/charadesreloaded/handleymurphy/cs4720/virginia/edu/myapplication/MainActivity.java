1f1be8fd4b9a61a4b63d3d2de94034e4490ee3fd
bhmurphy <bhm5zr@virginia.edu>
2018-04-12 19:15:43 -0400

    actually add main page with settings icon

--- app/src/main/java/charadesreloaded/handleymurphy/cs4720/virginia/edu/myapplication/MainActivity.java
+++ app/src/main/java/charadesreloaded/handleymurphy/cs4720/virginia/edu/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
 }
