ba5045acf2ad24a7f4a7cb514a28d26cb9748ebd
bhmurphy <bhm5zr@virginia.edu>
2018-04-16 19:07:09 -0400

    actual band aid

--- app/src/main/java/charadesreloaded/handleymurphy/cs4720/virginia/edu/myapplication/PlayActivity.java
+++ app/src/main/java/charadesreloaded/handleymurphy/cs4720/virginia/edu/myapplication/PlayActivity.java
@@ -120,11 +120,12 @@ public class PlayActivity extends AppCompatActivity implements SensorEventListen
                 //TODO: Remove these 2 lines, replace it with a check for card sets with 0 cards in the SelectCardSetToPlay Activity
                 if(mCards.size() == 0)
                     gameOver();
-
-                card.setText(mCards.get(mCardsPos));
-                timer.start();
-                gameBegun = true;
-                lastTime = System.currentTimeMillis();
+                else {
+                    card.setText(mCards.get(mCardsPos));
+                    timer.start();
+                    gameBegun = true;
+                    lastTime = System.currentTimeMillis();
+                }
             }
         }.start();
     }
